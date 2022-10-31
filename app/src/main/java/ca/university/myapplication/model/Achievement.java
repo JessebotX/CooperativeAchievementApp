e8ae3020bf95b426398409387b1f717710da300f
Jesse <username.git@posteo.net>
2022-10-31 13:21:50 -0700

    add Achievements enum
    possibly rename levels later once we come up with names?

---
+++ app/src/main/java/ca/university/myapplication/model/Achievement.java
@@ -0,0 +1,22 @@
+package ca.university.myapplication.model;
+
+/**
+ * TODO refactor their names
+ *
+ * Possible achievements that can be obtained.
+ *
+ * Level 0      < Expected Poor Score
+ * Level 1      ~ Expected Poor Score
+ * Level 8      >= Expected Great Score
+ */
+public enum Achievement {
+	LEVEL_0,
+	LEVEL_1,
+	LEVEL_2,
+	LEVEL_3,
+	LEVEL_4,
+	LEVEL_5,
+	LEVEL_6,
+	LEVEL_7,
+	LEVEL_8
+}
