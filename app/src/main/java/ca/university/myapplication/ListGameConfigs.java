3f0205ae156557f3d528c64133b7e36dbb190245
aidan <aidanzheng13@gmail.com>
2022-11-05 14:02:41 -0700

    Added click and long click functionality to the list view

--- app/src/main/java/ca/university/myapplication/ListGameConfigs.java
+++ app/src/main/java/ca/university/myapplication/ListGameConfigs.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -17,10 +19,49 @@ public class ListGameConfigs extends AppCompatActivity {
 
         setUpAddNewGameConfigButton();
         displayListOfConfigsUsingListView();
+        clickConfigToLaunchGameConfigInfo();
+        longClickConfigToOpenAnEditor();
+    }
+
+
+
+    //Displays info if there are no game configs added
+    //Lists game configs.
+    //Has a button to add a game config (launches the AddGameConfigActivity).
+    //On the click of a game config it launches the GameConfigInfoActivity
+    //On the long click of a game config it launches the AddGameConfigActivity with putting extras of the game config info to give the possibility of editing the game config.
+    //Has a delete button next to every game config that deletes the game config and updates the list view
+
+    private void longClickConfigToOpenAnEditor() {
+        ListView listView = findViewById(R.id.listOfConfigs);
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(ListGameConfigs.this, EditTheConfig.class);
+                startActivity(intent);
+                return false;
+            }
+        });
     }
 
     private void displayListOfConfigsUsingListView() {
+        /**
+         * if no configs we need to display a message
+         */
+    }
 
+    private void clickConfigToLaunchGameConfigInfo() {
+        ListView listView = findViewById(R.id.listOfConfigs);
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                /**
+                 * Intent intent = new Intent(ListGameConfigs.this, GameConfigInfoActivity.class);
+                 * startActivity(intent);
+                 */
+                return false;
+            }
+        });
     }
 
     private void setUpAddNewGameConfigButton() {
@@ -31,6 +72,7 @@ public class ListGameConfigs extends AppCompatActivity {
             public void onClick(View view) {
                 /**
                  * Intent intent = new Intent(ListGameConfigs.this, AddGameConfigActivity.class);
+                 * startActivity(intent);
                  */
             }
         });
