68c1e4800d8681f7783e1e8e19f7382ca985d046
Jesse <username.git@posteo.net>
2022-11-04 17:46:58 -0700

    add tests

---
+++ app/src/test/java/ca/university/myapplication/model/GameConfigManagerTest.java
@@ -0,0 +1,57 @@
+package ca.university.myapplication.model;
+
+import static org.junit.Assert.assertThrows;
+
+import org.junit.Test;
+
+/**
+ * Basic testing for GameConfigManager
+ */
+public class GameConfigManagerTest {
+	@Test
+	public void useSingleton() {
+		GameConfigManager manager = GameConfigManager.getInstance();
+
+		manager.addConfig("Hello world", 0, 7);
+		manager.getConfig(manager.totalConfigs() - 1).setName("Monopoly");
+		manager.removeConfig(0);
+
+		manager.addConfig("Monopoly", 0, Integer.MAX_VALUE);
+		manager.addConfig("Placeholder board game", 0, Integer.MAX_VALUE);
+
+		for (GameConfig config : manager) {
+			config.addGame(4, 100);
+		}
+
+		for (int i = manager.totalConfigs() - 1; i >= 0; i--) {
+			manager.removeConfig(i);
+		}
+	}
+
+	@Test
+	public void getConfigThrowsErrors() {
+		// should probably use singleton in production
+		GameConfigManager manager = new GameConfigManager();
+
+		manager.addConfig("Snakes and Ladders", 40, 100);
+		manager.getConfig(0).setName("Via Magica");
+
+		assertThrows(IllegalArgumentException.class, () -> manager.getConfig(1));
+		assertThrows(IllegalArgumentException.class, () -> manager.getConfig(-1));
+		assertThrows(IllegalArgumentException.class, () -> manager.getConfig(Integer.MAX_VALUE));
+	}
+
+	@Test
+	public void removeConfigThrowsErrors() {
+		// should probably use singleton in production
+		GameConfigManager manager = new GameConfigManager();
+
+		manager.addConfig("Snakes and Ladders", 40, 100);
+
+		assertThrows(IllegalArgumentException.class, () -> manager.removeConfig(-1));
+		assertThrows(IllegalArgumentException.class, () -> manager.removeConfig(1));
+		assertThrows(IllegalArgumentException.class, () -> manager.removeConfig(Integer.MAX_VALUE));
+
+		manager.removeConfig(0);
+	}
+}
