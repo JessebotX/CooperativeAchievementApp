dee1a43eab2e0f786eb5ff978014d25a7b1b81d9
Tomi Lui <64671485+6momo1@users.noreply.github.com>
2022-11-07 15:44:37 -0800

    added tab indentation. Reformatted code

--- app/src/main/java/ca/university/myapplication/AddGameActivity.java
+++ app/src/main/java/ca/university/myapplication/AddGameActivity.java
@@ -19,135 +19,139 @@ import ca.university.myapplication.model.GameConfigManager;
 
 public class AddGameActivity extends AppCompatActivity {
 
-    private static final String EXTRA_GAME_INDEX = "EXTRA_GAME_INDEX";
-    private static final String TAG = "ADD_GAME_ACTIVITY";
-    GameConfig gameConfig;
+	private static final String EXTRA_GAME_INDEX = "EXTRA_GAME_INDEX";
+	private static final String TAG = "ADD_GAME_ACTIVITY";
+	GameConfig gameConfig;
 
-    GameConfigManager gameConfigManager;
+	GameConfigManager gameConfigManager;
 
-    Game newGame;
+	Game newGame;
 
-    EditText inputNumPlayers;
-    EditText inputCombinedScore;
+	EditText inputNumPlayers;
+	EditText inputCombinedScore;
 
-    TextView tvAchievement;
+	TextView tvAchievement;
 
-    Button saveButton;
-
-
-    int numPlayers;
-    int combinedScore;
+	Button saveButton;
 
 
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_game);
-
-        inputNumPlayers = findViewById(R.id.inputNumPlayers);
-        inputCombinedScore = findViewById(R.id.inputCombinedScore);
-        tvAchievement = findViewById(R.id.tvAchievement);
-        saveButton = findViewById(R.id.btnSave);
-
-        gameConfigManager = GameConfigManager.getInstance();
-        // add fake data
-        gameConfigManager.addConfig("Poker", 10, 100);
-
-        // get Game index from caller
-        int gameIndex = extractDataFromIntent();
-        gameConfig = gameConfigManager.getConfig(gameIndex);
+	int numPlayers;
+	int combinedScore;
 
 
-        setUpSaveButton();
-        setUpInputListeners();
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_add_game);
 
-    /**
-     * For each input box, we want to add a listener to update and recalculate the achievement level
-     * everytime the user enters something.
-     */
-    private void setUpInputListeners() {
+		getSupportActionBar().setTitle("Add Game Info");
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        EditText[] userInputs = {inputNumPlayers, inputCombinedScore};
+		inputNumPlayers = findViewById(R.id.inputNumPlayers);
+		inputCombinedScore = findViewById(R.id.inputCombinedScore);
+		tvAchievement = findViewById(R.id.tvAchievement);
+		saveButton = findViewById(R.id.btnSave);
 
-        for (EditText userInput : userInputs) {
-            userInput.addTextChangedListener(new TextWatcher() {
-                public void afterTextChanged(Editable s) {}
-                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                }
-                public void onTextChanged(CharSequence s, int start, int before, int count) {
-                    Toast.makeText(AddGameActivity.this, "Detected num players", Toast.LENGTH_SHORT).show();
-                    refreshAchievementText();
-                }
-            });
-        }
-    }
+		gameConfigManager = GameConfigManager.getInstance();
+		// add fake data
+		gameConfigManager.addConfig("Poker", 10, 100);
 
-    private void setUpSaveButton() {
-        saveButton.setOnClickListener( view -> {
+		// get Game index from caller
+		int gameIndex = extractDataFromIntent();
+		gameConfig = gameConfigManager.getConfig(gameIndex);
 
-            String numPlayersText = inputNumPlayers.getText().toString();
-            String combinedScoreText = inputNumPlayers.getText().toString();
 
-            // exit early if input is not entered
-            if ( !isInt(numPlayersText) || !isInt(combinedScoreText) || Integer.parseInt(numPlayersText) < 1) {
-                Toast.makeText(this, "Please Finish Entering Inputs.", Toast.LENGTH_SHORT).show();
-                return;
-            }
+		setUpSaveButton();
+		setUpInputListeners();
+	}
 
-            numPlayers = Integer.parseInt(numPlayersText);
-            combinedScore = Integer.parseInt(combinedScoreText);
+	/**
+	 * For each input box, we want to add a listener to update and recalculate the achievement level
+	 * everytime the user enters something.
+	 */
+	private void setUpInputListeners() {
 
-            // add to config
+		EditText[] userInputs = {inputNumPlayers, inputCombinedScore};
 
-            gameConfig.addGame(numPlayers, combinedScore);
+		for (EditText userInput : userInputs) {
+			userInput.addTextChangedListener(new TextWatcher() {
+				public void afterTextChanged(Editable s) {
+				}
 
-            Toast.makeText(this, "New Game Saved!", Toast.LENGTH_SHORT).show();
-            finish();
-        });
-    }
+				public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+				}
 
-    private void refreshAchievementText() {
-        String numPlayersText = inputNumPlayers.getText().toString();
-        String combinedScoreText = inputCombinedScore.getText().toString();
+				public void onTextChanged(CharSequence s, int start, int before, int count) {
+					Toast.makeText(AddGameActivity.this, "Detected num players", Toast.LENGTH_SHORT).show();
+					refreshAchievementText();
+				}
+			});
+		}
+	}
 
-        if (!isInt(numPlayersText) || !isInt(combinedScoreText) || Integer.parseInt(numPlayersText) < 1) {
-            return;
-        }
+	private void setUpSaveButton() {
+		saveButton.setOnClickListener(view -> {
 
-        int numPlayers = Integer.parseInt(numPlayersText);
-        int numCombinedScore = Integer.parseInt(combinedScoreText);
+			String numPlayersText = inputNumPlayers.getText().toString();
+			String combinedScoreText = inputNumPlayers.getText().toString();
 
-        int achievementLevel = calculateAchievementLevel(numPlayers, numCombinedScore);
-        // update text
-        tvAchievement.setText(Integer.toString(achievementLevel));
-    }
+			// exit early if input is not entered
+			if (!isInt(numPlayersText) || !isInt(combinedScoreText) || Integer.parseInt(numPlayersText) < 1) {
+				Toast.makeText(this, "Please Finish Entering Inputs.", Toast.LENGTH_SHORT).show();
+				return;
+			}
 
-    private int calculateAchievementLevel(int numPlayers, int combinedScore) {
-        newGame = new Game(numPlayers, combinedScore, gameConfig.getExpectedPoorScore(), gameConfig.getExpectedGreatScore());
-        return newGame.getAchievementLevel();
-    }
+			numPlayers = Integer.parseInt(numPlayersText);
+			combinedScore = Integer.parseInt(combinedScoreText);
 
-    public static boolean isInt(String str) {
-        try {
-            @SuppressWarnings("unused")
-            int x = Integer.parseInt(str);
-            return true; //String is an Integer
-        } catch (NumberFormatException e) {
-            return false; //String is not an Integer
-        }
+			// add to config
 
-    }
+			gameConfig.addGame(numPlayers, combinedScore);
 
-    public static Intent makeIntent(Context context, int gameIndex){
-        Intent intent = new Intent(context, AddGameActivity.class);
-        intent.putExtra(EXTRA_GAME_INDEX, gameIndex);
-        return intent;
-    }
+			Toast.makeText(this, "New Game Saved!", Toast.LENGTH_SHORT).show();
+			finish();
+		});
+	}
 
-    private int extractDataFromIntent() {
-        Intent intent = getIntent();
-        return intent.getIntExtra(EXTRA_GAME_INDEX, -1);
-    }
+	private void refreshAchievementText() {
+		String numPlayersText = inputNumPlayers.getText().toString();
+		String combinedScoreText = inputCombinedScore.getText().toString();
+
+		if (!isInt(numPlayersText) || !isInt(combinedScoreText) || Integer.parseInt(numPlayersText) < 1) {
+			return;
+		}
+
+		int numPlayers = Integer.parseInt(numPlayersText);
+		int numCombinedScore = Integer.parseInt(combinedScoreText);
+
+		int achievementLevel = calculateAchievementLevel(numPlayers, numCombinedScore);
+		// update text
+		tvAchievement.setText(Integer.toString(achievementLevel));
+	}
+
+	private int calculateAchievementLevel(int numPlayers, int combinedScore) {
+		newGame = new Game(numPlayers, combinedScore, gameConfig.getExpectedPoorScore(), gameConfig.getExpectedGreatScore());
+		return newGame.getAchievementLevel();
+	}
+
+	public static boolean isInt(String str) {
+		try {
+			@SuppressWarnings("unused")
+			int x = Integer.parseInt(str);
+			return true; //String is an Integer
+		} catch (NumberFormatException e) {
+			return false; //String is not an Integer
+		}
+	}
+
+	public static Intent makeIntent(Context context, int gameIndex) {
+		Intent intent = new Intent(context, AddGameActivity.class);
+		intent.putExtra(EXTRA_GAME_INDEX, gameIndex);
+		return intent;
+	}
+
+	private int extractDataFromIntent() {
+		Intent intent = getIntent();
+		return intent.getIntExtra(EXTRA_GAME_INDEX, -1);
+	}
 }
