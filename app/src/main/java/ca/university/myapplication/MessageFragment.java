2bfe005d6e823f0d1dcc1eafc8bbb68c745d08aa
natha <nka66@sfu.ca>
2022-11-19 22:12:26 -0800

    Put in animation and close button
    translation and fade animation and a close button

--- app/src/main/java/ca/university/myapplication/MessageFragment.java
+++ app/src/main/java/ca/university/myapplication/MessageFragment.java
@@ -2,21 +2,17 @@ package ca.university.myapplication;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.DialogInterface;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDialogFragment;
 
 public class MessageFragment extends AppCompatDialogFragment {
-//	Animation tv_animation;
 	@NonNull
 	@Override
 	public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
@@ -24,29 +20,22 @@ public class MessageFragment extends AppCompatDialogFragment {
 		View v = LayoutInflater.from(getActivity())
 				.inflate(R.layout.achievement_celebration,null);
 
+		//setup close button
+		ImageView close_button = v.findViewById(R.id.btn_close);
+		close_button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				getActivity().finish();
+			}
+		});
+
 		//play sound
 		final MediaPlayer mediaPlayer = MediaPlayer.create(getActivity(),R.raw.celebration);
 		mediaPlayer.start();
 
-		//animation
-//		tv_animation = AnimationUtils.loadAnimation(getActivity(),R.anim.celebration_tv_animation);
-//		TextView tv_celebration = getActivity().findViewById(R.id.tv_celebration);
-//		tv_celebration.setAnimation(tv_animation);
-
-		//create a button listener
-		DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
-			@Override
-			public void onClick(DialogInterface dialogInterface, int i) {
-				getActivity().finish();
-			}
-		};
-
 		//build the alert dialog
-		return new AlertDialog.Builder(getActivity())
-//				.setTitle("Congratulations!")
+		return new AlertDialog.Builder(getActivity(),R.style.dialog)
 				.setView(v)
-				.setPositiveButton(android.R.string.ok,listener)
 				.create();
-
 	}
 }
