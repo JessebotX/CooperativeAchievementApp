a7a44e0bfbec3ba2d16a62768da927a1072d4611
natha <nka66@sfu.ca>
2022-11-29 01:32:52 -0800

    added histogram in layout
    added needed repositories in build.gradle, edited settings in settings.gradle and added histogram in layout

--- app/src/main/java/ca/university/myapplication/AchievementStatsActivity.java
+++ app/src/main/java/ca/university/myapplication/AchievementStatsActivity.java
@@ -11,7 +11,7 @@ public class AchievementStatsActivity extends AppCompatActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_histogram);
+		setContentView(R.layout.activity_achievement_stats);
 	}
 
 	public static Intent makeIntent(Context context) {
