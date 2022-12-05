8a6133cf4e88e809e60d36cbde313506a35853a0
natha <nka66@sfu.ca>
2022-12-04 18:38:28 -0800

    layout ready and replay button implemented
    added text views and spinner, replay button to replay the dialog

--- app/src/main/java/ca/university/myapplication/MessageFragment.java
+++ app/src/main/java/ca/university/myapplication/MessageFragment.java
@@ -7,10 +7,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDialogFragment;
+import androidx.fragment.app.FragmentManager;
 
 public class MessageFragment extends AppCompatDialogFragment {
 	@NonNull
@@ -28,6 +30,22 @@ public class MessageFragment extends AppCompatDialogFragment {
 				getActivity().finish();
 			}
 		});
+
+		//setup replay button
+		ImageView replay_button = v.findViewById(R.id.btn_replay);
+		replay_button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				getDialog().cancel();
+				FragmentManager manager = getActivity().getSupportFragmentManager();
+				MessageFragment dialog = new MessageFragment();
+				dialog.show(manager,"Celebration!");
+			}
+		});
+
+		//text view to show next level points
+		TextView tv_next_level = v.findViewById(R.id.tv_next_level_score);
+		tv_next_level.setText(getString(R.string.points_for_next_achievement,7,"hi"));
 
 		//play sound
 		final MediaPlayer mediaPlayer = MediaPlayer.create(getActivity(),R.raw.celebration);
