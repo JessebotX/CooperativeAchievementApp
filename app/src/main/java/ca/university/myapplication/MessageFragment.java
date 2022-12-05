2d0c5dc2aea7829b92983f0366498baa361644ff
natha <nka66@sfu.ca>
2022-12-05 13:12:14 -0800

    refactored code to look tidier

--- app/src/main/java/ca/university/myapplication/MessageFragment.java
+++ app/src/main/java/ca/university/myapplication/MessageFragment.java
@@ -22,9 +22,8 @@ import ca.university.myapplication.model.Game;
 import ca.university.myapplication.model.GameConfigManager;
 
 public class MessageFragment extends AppCompatDialogFragment {
+	private View v;
 	private GameConfigManager manager;
-	private Game savedGame;
-	private int[] requiredScores;
 	private String[][] achievementNames;
 
 	@NonNull
@@ -33,71 +32,14 @@ public class MessageFragment extends AppCompatDialogFragment {
 		manager = GameConfigManager.getInstance();
 
 		//create the view to show
-		View v = LayoutInflater.from(getActivity())
+		v = LayoutInflater.from(getActivity())
 				.inflate(R.layout.achievement_celebration,null);
 
-		//setup close button
-		ImageView close_button = v.findViewById(R.id.btn_close);
-		close_button.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				getActivity().finish();
-			}
-		});
-
-		//setup replay button
-		ImageView replay_button = v.findViewById(R.id.btn_replay);
-		replay_button.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				getDialog().cancel();
-				FragmentManager manager = getActivity().getSupportFragmentManager();
-				MessageFragment dialog = new MessageFragment();
-				dialog.show(manager,"Celebration!");
-			}
-		});
-
-		//initialize achievement names
-		achievementNames = new String[][] {
-				getResources().getStringArray(R.array.achievement_theme_animals),
-				getResources().getStringArray(R.array.achievement_theme_resources),
-				getResources().getStringArray(R.array.achievement_theme_weapons)
-		};
-
-		//fill text view to show next level points
-		savedGame = AddGameActivity.getNewGame();
-		if (savedGame.getAchievementLevel() < 8) {
-			requiredScores = savedGame.getAchievementLevelRequiredScores();
-			int nextLevelRequiredScore = requiredScores[savedGame.getAchievementLevel()];
-			int neededPoints = nextLevelRequiredScore - savedGame.getTotalScore();
-
-			String nextLevelName = achievementNames[manager.getTheme()][savedGame.getAchievementLevel() + 1];
-			TextView tv_next_level = v.findViewById(R.id.tv_next_level_score);
-			tv_next_level.setText(getString(R.string.points_for_next_achievement,
-					neededPoints, nextLevelName));
-		}
-
-		//theme spinner
-		Spinner themeSpinner = v.findViewById(R.id.spinner_theme);
-		ArrayAdapter themeAdapter = ArrayAdapter.createFromResource
-				(getActivity(), R.array.achievement_themes, R.layout.theme_spinner_text);
-		themeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
-		themeSpinner.setAdapter(themeAdapter);
-//		themeSpinner.setSelection(lastSizeClick);
-		themeSpinner.setSelection(manager.getTheme());
-		themeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-			@Override
-			public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
-//				editor.putInt("lastSizeClick",position).commit();
-				manager.setTheme(position);
-				saveIntToSharedPrefs(ListGameConfigsActivity.SAVED_THEME_ID_KEY, position);
-			}
-
-			@Override
-			public void onNothingSelected(AdapterView<?> adapterView) {
-
-			}
-		});
+		setupCloseButton();
+		setupReplayButton();
+		initializeAchievementNames();
+		fillNextLevelPointsTextView();
+		populateThemeSpinner();
 
 		//play sound
 		final MediaPlayer mediaPlayer = MediaPlayer.create(getActivity(),R.raw.celebration);
@@ -115,5 +57,76 @@ public class MessageFragment extends AppCompatDialogFragment {
 
 		editor.putInt(key, num);
 		editor.apply();
+	}
+
+	//setup close button
+	private void setupCloseButton() {
+		ImageView close_button = v.findViewById(R.id.btn_close);
+		close_button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				getActivity().finish();
+			}
+		});
+	}
+
+	//setup replay button
+	private void setupReplayButton() {
+		ImageView replay_button = v.findViewById(R.id.btn_replay);
+		replay_button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				getDialog().cancel();
+				FragmentManager manager = getActivity().getSupportFragmentManager();
+				MessageFragment dialog = new MessageFragment();
+				dialog.show(manager, "Celebration!");
+			}
+		});
+	}
+
+	//initialize achievement names
+	private void initializeAchievementNames() {
+		achievementNames = new String[][]{
+				getResources().getStringArray(R.array.achievement_theme_animals),
+				getResources().getStringArray(R.array.achievement_theme_resources),
+				getResources().getStringArray(R.array.achievement_theme_weapons)
+		};
+	}
+
+	//fill text view to show next level points
+	private void fillNextLevelPointsTextView() {
+		Game savedGame = AddGameActivity.getNewGame();
+		if (savedGame.getAchievementLevel() < 8) {
+			int[] requiredScores = savedGame.getAchievementLevelRequiredScores();
+			int nextLevelRequiredScore = requiredScores[savedGame.getAchievementLevel()];
+			int neededPoints = nextLevelRequiredScore - savedGame.getTotalScore();
+
+			String nextLevelName = achievementNames[manager.getTheme()][savedGame.getAchievementLevel() + 1];
+			TextView tv_next_level = v.findViewById(R.id.tv_next_level_score);
+			tv_next_level.setText(getString(R.string.points_for_next_achievement,
+					neededPoints, nextLevelName));
+		}
+	}
+
+	//theme spinner
+	private void populateThemeSpinner() {
+		Spinner themeSpinner = v.findViewById(R.id.spinner_theme);
+		ArrayAdapter themeAdapter = ArrayAdapter.createFromResource
+				(getActivity(), R.array.achievement_themes, R.layout.theme_spinner_text);
+		themeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+		themeSpinner.setAdapter(themeAdapter);
+		themeSpinner.setSelection(manager.getTheme());
+		themeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+			@Override
+			public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+				manager.setTheme(position);
+				saveIntToSharedPrefs(ListGameConfigsActivity.SAVED_THEME_ID_KEY, position);
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> adapterView) {
+
+			}
+		});
 	}
 }
