138289076a5f2ef1751d348739f8d1fc2546c728
aidan <aidanzheng13@gmail.com>
2022-12-05 22:05:59 -0800

    experimental code for accessing camera and saving the picture

--- app/src/main/java/ca/university/myapplication/AddGameConfigActivity.java
+++ app/src/main/java/ca/university/myapplication/AddGameConfigActivity.java
@@ -1,13 +1,19 @@
 package ca.university.myapplication;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,6 +24,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.cast.framework.media.ImagePicker;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -27,7 +35,11 @@ import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.google.gson.Gson;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.UUID;
 
 import ca.university.myapplication.model.GameConfig;
@@ -51,6 +63,9 @@ public class AddGameConfigActivity extends AppCompatActivity {
 	ImageView configImage;
 
 	static final int REQUEST_IMAGE_CAPTURE = 1;
+	static final int CAMERA_PERMISSION_REQUEST_CODE = 1;
+	static final int CAMERA_REQUEST_CODE = 2;
+	static final int COMPRESSION_QUALITY = 100;
 
 
 	@Override
@@ -64,6 +79,7 @@ public class AddGameConfigActivity extends AppCompatActivity {
 		androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
 
 		setSupportActionBar(toolbar);
+		setupCamera();
 
 		ActionBar ab = getSupportActionBar();
 		ab.setDisplayHomeAsUpEnabled(true);
@@ -95,29 +111,95 @@ public class AddGameConfigActivity extends AppCompatActivity {
 		uploadBtn.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
-				dispatchTakePictureIntent();
+				//dispatchTakePictureIntent();
 			}
 		});
 	}
 	//4-----------------------------------------------------------
-	private void dispatchTakePictureIntent() {
-		Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-		try {
-			startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-		} catch (ActivityNotFoundException e) {
-			Toast.makeText(getApplicationContext(), "ERROR", Toast.LENGTH_LONG).show();
+	//code used from developer.android.com:
+	//https://developer.android.com/training/camera-deprecated/photobasics
+
+	//	private void dispatchTakePictureIntent() {
+	//		Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+	//		try {
+	//			startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+	//		} catch (ActivityNotFoundException e) {
+	//			Toast.makeText(getApplicationContext(), "ERROR", Toast.LENGTH_LONG).show();
+	//		}
+	//	}
+	//
+	//	@Override
+	//	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	//		super.onActivityResult(requestCode, resultCode, data);
+	//
+	//		if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+	//			Bundle extras = data.getExtras();
+	//			Bitmap imageBitmap = (Bitmap) extras.get("data");
+	//			configImage.setImageBitmap(imageBitmap);
+	//
+	//
+	//		}
+	//	}
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+		super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+		if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+			if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+				Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+				startActivityForResult(intent, CAMERA_REQUEST_CODE);
+			} else {
+				Toast.makeText(this, "Must allow camera access to take photo", Toast.LENGTH_SHORT).show();
+			}
 		}
 	}
 
 	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
 
-		if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-			Bundle extras = data.getExtras();
-			Bitmap imageBitmap = (Bitmap) extras.get("data");
-			configImage.setImageBitmap(imageBitmap);
+		if (resultCode == Activity.RESULT_OK) {
+			if (requestCode == CAMERA_REQUEST_CODE) {
+				if (data == null || data.getExtras() == null) {
+					Toast.makeText(this, "Error has occured with getting image from camera", Toast.LENGTH_SHORT).show();
+				}
+				Bitmap photo = (Bitmap)data.getExtras().get("data");
+				configImage = findViewById(R.id.gameConfigImage);
+
+				String photoBase64 = bitmapToBase64(photo);
+				Bitmap photoBitmap = base64ToBitmap(photoBase64);
+
+				configImage.setImageBitmap(photoBitmap);
+			}
 		}
+	}
+
+	private String bitmapToBase64(Bitmap bitmap) {
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		bitmap.compress(Bitmap.CompressFormat.JPEG, COMPRESSION_QUALITY, outputStream);
+		return Base64.encodeToString(outputStream.toByteArray(), Base64.DEFAULT);
+	}
+
+	private Bitmap base64ToBitmap(String base64) {
+		byte[] decodedString = Base64.decode(base64, Base64.DEFAULT);
+		return BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+	}
+
+	private void setupCamera() {
+		//Button buttonCamera = findViewById(R.id.buttonCamera);
+		uploadBtn = findViewById(R.id.uploadImageBtn);
+		String[] requestedPermissions = new String[] { Manifest.permission.CAMERA };
+
+		uploadBtn.setOnClickListener(e -> {
+			if (ContextCompat.checkSelfPermission(AddGameConfigActivity.this, Manifest.permission.CAMERA)
+					== PackageManager.PERMISSION_GRANTED)
+			{
+				Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+				startActivityForResult(intent, CAMERA_REQUEST_CODE);
+			} else {
+				ActivityCompat.requestPermissions(AddGameConfigActivity.this, requestedPermissions, CAMERA_PERMISSION_REQUEST_CODE);
+			}
+		});
 	}
 	//4-----------------------------------------------------------
 	private void extractGameConfigExtra() {
